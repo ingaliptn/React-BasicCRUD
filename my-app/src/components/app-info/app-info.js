3b14813a53ffc55cad270e254928d7808de332cc
Mykyta Tkachuk <nikitatkachuk6@gmail.com>
2023-04-03 16:29:56 +0300

    Передача товариства Соні Котюк

--- my-app/src/components/app-info/app-info.js
+++ my-app/src/components/app-info/app-info.js
@@ -3,7 +3,7 @@ import "./app-info.css";
 const AppInfo = ({ employers, increased, risePerson }) => {
   return (
     <div className="app-info">
-      <h1>Бухгалтерський облік працівників у компанії "Амогус"</h1>
+      <h1>Бухгалтерський облік працівників у компанії ФОП "Софія Котюк"</h1>
       <h2>Загальна кількість працівників: {employers}</h2>
       <h2>Працівників отримають премію: {increased}</h2>
       <h2>Працівники йдуть на підвищення: {risePerson}</h2>
