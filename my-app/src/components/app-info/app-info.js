a23eb3627eaec7b835add06c734500caf53efce1
Mykyta Tkachuk <nikitatkachuk6@gmail.com>
2023-04-03 15:53:56 +0300

    Text translate

--- my-app/src/components/app-info/app-info.js
+++ my-app/src/components/app-info/app-info.js
@@ -3,10 +3,10 @@ import "./app-info.css";
 const AppInfo = ({employers, increased, risePerson}) => {
   return (
     <div className="app-info">
-      <h1>Учет сотрудников в компании "Амогус"</h1>
-      <h2>Общее число сотрудников: {employers}</h2>
-      <h2>Премию получат: {increased}</h2>
-      <h2>На повышение идут: {risePerson}</h2>
+      <h1>Бухгалтерський облік працівників у компанії "Амогус"</h1>
+      <h2>Загальна кількість працівників: {employers}</h2>
+      <h2>Вони отримають приз: {increased}</h2>
+      <h2>Вони йдуть на збільшення: {risePerson}</h2>
     </div>
   );
 };
